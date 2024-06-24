6c0147ba1e722f6ac673f540bd20fab0b5995824
Prado3 <lukasprado8@gmail.com>
2024-06-24 19:20:14 -0300

    1

--- contracts/JamToken.sol
+++ contracts/JamToken.sol
@@ -19,10 +19,44 @@ contract JamToken {
     event Approval (
         address indexed _owner,
         address indexed _spender,
-        uint256 value
+        uint256 _value
     );
 
     // Estructuras de datos
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
+
+    // Constructor
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    // Transferencia de tokens de un usuario
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    // Aprobacion de una cantidad para ser gastada por un operador
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // Transferencia de tokens especificando el emisor
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
 }
